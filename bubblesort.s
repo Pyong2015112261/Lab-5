627eb1bb45ead808c71ecf5eb6e4b6f41f13b32f
LAPTOP-TFQ265EP\pauly <paulyong42@yahoo.com>
2018-10-29 19:39:14 -0600

    For Dalwin Lewis

--- bubblesort.s
+++ bubblesort.s
@@ -8,52 +8,52 @@ Paul Yong, Shelby Castillo
 
 
 BubbleSort:
-  PUSH  {R3-R7, LR}             ;; Save Context in the registers we will use.
-  MOV   R3,#+1                  ;; set swapped register as true
+  PUSH  {R3-R7, LR}             
+  MOV   R3,#+1                  
 
 OuterLoop:
-  CMP   R3,#+1                  ;; check if swapped register is true
-  BNE   FINISH                  ;; if (swapped == false) -> finish
-  MOV   R3,#+0                  ;; set swapped register as false
-  MOV   R4,#+1                  ;; initialize for loop to start at i = 1
-  B     InnerLoop               ;; go to the inner loop
+  CMP   R3,#+1                  
+  BNE   FINISH                  
+  MOV   R3,#+0                  
+  MOV   R4,#+1                  
+  B     InnerLoop               
   
 InnerLoop:  
-  CMP   R4,R1                   ;; check for more elements r4 -> i index of array
-  BNE   Compare_Array           ;; if not at the end of the foor loop compare elements in array
-  B     OuterLoop               ;; once inner loop completes go back to outer loop
+  CMP   R4,R1                   
+  BNE   Compare_Array           
+  B     OuterLoop               
 
 Compare_Array:
-  SUB   R5,R4,#+1               ;; r5 <- i - 1
-  LDR   R6,[R0,R5,LSL #+2]      ;; r6 <- A[r5]
-  LDR   R7,[R0,R4,LSL #+2]      ;; r7 <- A[r4]
-  CMP   R2,#+1                  ;; compare to see if ascending flag is set    
+  SUB   R5,R4,#+1               
+  LDR   R6,[R0,R5,LSL #+2]      
+  LDR   R7,[R0,R4,LSL #+2]     
+  CMP   R2,#+1                    
   BEQ   Ascending
   BNE   Descending
   
 Ascending:
-  CMP   R6,R7                   ;; compare  A[i-1] > A[i]
+  CMP   R6,R7                   
   B     Swap_and_Increment
   
 Descending:
-  CMP   R7,R6                   ;; compare  A[i-1] < A[i] 
+  CMP   R7,R6                   
   B     Swap_and_Increment
 
 Swap_and_Increment:
-  BGT   Swap                    ;; if flags are set from either Ascending or Descending labels
-  B     Increment               ;; increment array index
+  BGT   Swap                   
+  B     Increment              
 
 Swap:
-  STR   R6,[R0, R4, LSL #+2]    ;; A[r6 ]-> @(r0 [r4 << 2] word shift
-  STR   R7,[R0, R5, LSL #+2]    ;; A[r7] -> @(r0 [r5 << 2] word shift
-  MOV   R3,#+1                  ;; set swapped register as true
-  B Increment                   ;; increment array index
+  STR   R6,[R0, R4, LSL #+2]    
+  STR   R7,[R0, R5, LSL #+2]   
+  MOV   R3,#+1                 
+  B Increment                 
   
 Increment:
-  ADD   R4,R4,#+1               ;; Next element number
-  B     InnerLoop               ;; repeat
+  ADD   R4,R4,#+1               
+  B     InnerLoop               
   
 FINISH:  
-  POP   {R3-R7}                 ;; restore context
-  MOV   pc,lr                   ;; return
+  POP   {R3-R7}                
+  MOV   pc,lr                   
   END
